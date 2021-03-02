21aeedb4239345aaad64c513839e7431f878b6aa
bakyvelmammedov <bakyvelmammedov@gmail.com>
2021-03-03 00:27:08 +0400

    created changeAvatar.feature file

--- src/test/java/com/group7Project/runners/CukesRunner.java
+++ src/test/java/com/group7Project/runners/CukesRunner.java
@@ -11,8 +11,8 @@ import org.junit.runner.RunWith;
                 "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "com/group7Project/step_definitions",
-        dryRun = false
-        //tags = "@links"
+        dryRun = true,
+        tags = "@changeAvatar"
 )
 public class CukesRunner {
 
--- src/test/java/com/group7Project/step_definitions/AvatarStepDefinitions.java
+++ src/test/java/com/group7Project/step_definitions/AvatarStepDefinitions.java
@@ -1,7 +1,7 @@
 package com.group7Project.step_definitions;
 
 import com.group7Project.pages.DashboardPage;
-import io.cucumber.java.en.Then;
+import io.cucumber.java.en.*;
 
 public class AvatarStepDefinitions {
 
@@ -9,7 +9,23 @@ public class AvatarStepDefinitions {
 
     @Then("User should be able to see their avatars")
     public void user_should_be_able_to_see_their_avatars() {
+
         dashboard.verifyAvatarDisplay();
+    }
+
+    @When("{string} navigates to preferences page")
+    public void navigates_to_preferences_page(String string) {
+
+    }
+
+    @When("select new picture for avatar")
+    public void select_new_picture_for_avatar() {
+
+    }
+
+    @Then("Verify that picture is changed")
+    public void verify_that_picture_is_changed() {
+
     }
 
 }
