d2815c77592b7f3da70a2e7919b5325e30b6a31f
Imdat K <imdatkucuk28@gmail.com>
2021-03-10 15:00:03 +0100

    Created ProjectPage, Created StepsDefs. Created Features.

--- src/test/java/com/group7Project/step_definitions/NumberOfProjectStepsDefs.java
+++ src/test/java/com/group7Project/step_definitions/NumberOfProjectStepsDefs.java
@@ -11,8 +11,8 @@ ProjectPage projectPage=new ProjectPage();
     @Then("the user should be able to see exact total number of projects")
     public void the_user_should_be_able_to_see_exact_total_number_of_projects() {
         int expectedProjectNumber = projectPage.ListingTheProject().size();
-//        int actualProjectNumber = Integer.parseInt( projectPage.numberOfProjects.getText() );
-//        Assert.assertEquals( expectedProjectNumber, actualProjectNumber )
+    int actualProjectNumber = Integer.parseInt( projectPage.numberOfProjects.getText() );
+    Assert.assertEquals( expectedProjectNumber, actualProjectNumber );
 
     }
 }
